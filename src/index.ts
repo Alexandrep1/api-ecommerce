cfa0c4608b81b3326974957ee9c1f3ee31be62ef
Alexandre Pinto <alexandrepinto199014@hotmail.ocom>
2025-07-02 09:20:04 -0300

    commit

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,10 @@ import express from "express";
 const app = express()
 app.use(express.json())
 
-app.get("/", (req, res) => {res.send("servidor rodando");})
+app.get("/", (req, res) => {
+    res.send("servidor rodando")
+})
+
+app.listen(3000, () => {
+    console.log("servidor rodando")
+})
