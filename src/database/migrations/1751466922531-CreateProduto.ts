b745b72f759f8b08cf013054e70fa2a3c892ea59
Alexandre Pinto <alexandrepinto199014@hotmail.ocom>
2025-07-02 11:43:04 -0300

    utilizando migrations

--- src/database/migrations/1751466922531-CreateProduto.ts
+++ src/database/migrations/1751466922531-CreateProduto.ts
@@ -1,8 +1,22 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateProduto1751466922531 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "produtos",
+                columns: [
+                    {
+                        name: "id",
+                        type: "int",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment"
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
