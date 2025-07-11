ef5854530bf40b6c5ed7ec093e46d9c567ae6d86
alexandre <alexandrepinto199014@hotmail.com>
2025-07-11 09:36:17 -0300

    criando autenticador do usuario

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { AuthService } from "../routes/AuthService";
+
+
+export const AuthController = {
+
+    async login(req: Request, res: Response): Promise<void> {
+        const [email, password] = req.body
+
+        try {
+            const auth = AuthService.auth(email, password)
+            res.json(auth)
+        } catch (error: any) {
+            res.status(401).json({erro: error.message})
+            console.log(error);
+            
+        }
+    }
+}
